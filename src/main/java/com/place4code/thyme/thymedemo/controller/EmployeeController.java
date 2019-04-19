742357fd2f254b570249876863e4006a49072240
place4code <place4code@gmail.com>
2019-04-19 17:37:11 +0200

    created entity and view for employees

--- src/main/java/com/place4code/thyme/thymedemo/controller/EmployeeController.java
+++ src/main/java/com/place4code/thyme/thymedemo/controller/EmployeeController.java
@@ -10,7 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.place4code.thyme.thymedemo.model.Employee;
+import com.place4code.thyme.thymedemo.entity.Employee;
+
 
 @Controller
 @RequestMapping("employees")
