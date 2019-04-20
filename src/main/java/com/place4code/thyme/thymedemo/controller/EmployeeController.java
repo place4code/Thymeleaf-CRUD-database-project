d0ad435d98643a24995a69664db9b31ff1cbdc63
place4code <place4code@gmail.com>
2019-04-20 17:45:14 +0200

    list mapping ang save mapping, new view for save employee

--- src/main/java/com/place4code/thyme/thymedemo/controller/EmployeeController.java
+++ src/main/java/com/place4code/thyme/thymedemo/controller/EmployeeController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.place4code.thyme.thymedemo.entity.Employee;
 import com.place4code.thyme.thymedemo.service.EmployeeService;
 
 
@@ -30,7 +33,30 @@ public class EmployeeController {
 		model.addAttribute("employees", employeeService.findAll());
 		
 		//return view with list of employees
-		return "list-employees";
+		return "employees/list";
 		
 	}
+	
+	@GetMapping("/showAddForm")
+	public String showAddForm(Model model) {
+		
+		//add attribute
+		model.addAttribute("employee", new Employee());
+		
+		//return form view
+		return "employees/save";
+		
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		
+		//save employee
+		employeeService.save(employee);
+		//redirect to list
+		return "redirect:/employees/list";
+		
+	}
+	
+	
 }
