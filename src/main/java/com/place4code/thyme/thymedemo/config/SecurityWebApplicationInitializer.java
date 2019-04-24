c6f8311d7ad3d0236833a4c90a31de9b953aedf1
place4code <place4code@gmail.com>
2019-04-24 14:10:56 +0200

    add Spring Security step 1

---
+++ src/main/java/com/place4code/thyme/thymedemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.place4code.thyme.thymedemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
